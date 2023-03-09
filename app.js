6d97968c84914fef4b1922a60e2828218dfc259f
adnan23062000 <bsse1131@iit.du.ac.bd>
2023-03-09 20:25:56 +0600

    router reconfigured

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const app = express();
 
 const userRouter = require("./router/user.router");
 
+const indexRouter2 = require("./router");
+
 
 app.use(express.json());
 
-app.use("/users", userRouter);
+app.use('/', indexRouter2);
 
 app.listen(process.env.APP_PORT, () => {
     console.log("server up and running on PORT: ", process.env.APP_PORT);
--- router/user.router.js
+++ router/user.router.js
@@ -1,10 +1,15 @@
 const { createUser, getUserById, getUsers, updateUser, deleteUser } = require("../controller/user.controller");
 const router = require("express").Router();
 
-router.post("/", createUser);
-router.get("/", getUsers);
-router.get("/:userName", getUserById);
-router.put("/:userName", updateUser);
-router.delete("/:userName", deleteUser);
+router.route('/')
+    .post(createUser)
+    .get(getUsers);
+
+
+router.route('/:userName')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
 
 module.exports = router;
